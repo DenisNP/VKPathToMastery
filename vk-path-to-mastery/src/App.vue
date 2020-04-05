b7a0b4de1e7795ad7b1a5c4f7e8c893ccd525385
Denis Peshekhonov <denis@peshekhonov.ru>
2020-04-05 05:44:54 +0300

    client layout

--- vk-path-to-mastery/src/App.vue
+++ vk-path-to-mastery/src/App.vue
@@ -1,7 +1,37 @@
 <template>
-    <div id="app">
-    </div>
+    <f7-app :params="f7params">
+        <f7-view main class="safe-areas">
+            <f7-page>
+                <f7-navbar title="Путь к мастерству"></f7-navbar>
+            </f7-page>
+        </f7-view>
+    </f7-app>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            f7params: {
+                theme: 'auto',
+                name: 'Путь к мастерству',
+                id: 'net.denisnp.pathtomastery',
+            },
+        };
+    },
+};
+</script>
+
 <style>
+    :root {
+        --f7-theme-color: #b20101;
+        --f7-theme-color-rgb: 178, 1, 1;
+        --f7-theme-color-shade: #890101;
+        --f7-theme-color-tint: #db0101;
+        --f7-bars-bg-color: var(--f7-theme-color);
+        --f7-bars-bg-color-rgb: var(--f7-theme-color-rgb);
+        --f7-bars-translucent-opacity: 0.9;
+        --f7-bars-text-color: #ffde00;
+        --f7-bars-link-color: #ffde00;
+    }
 </style>
--- vk-path-to-mastery/src/main.js
+++ vk-path-to-mastery/src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
+import Framework7 from 'framework7/framework7-lite.esm.bundle';
+import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle';
+import 'framework7/css/framework7.bundle.css';
+
 import App from './App.vue';
-import router from './router';
 import store from './store';
+
+Framework7.use(Framework7Vue);
 
 Vue.config.productionTip = false;
 
 new Vue({
-    router,
     store,
     render: (h) => h(App),
 }).$mount('#app');
